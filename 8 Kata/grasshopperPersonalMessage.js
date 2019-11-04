28d68a3ab5979d7a5d1c7aee58b7149d85a6f01b
Jonah Latham <jonah.latham21@gmail.com>
2019-11-04 15:36:45 -0700

    code

---
+++ 8 Kata/grasshopperPersonalMessage.js
@@ -0,0 +1,12 @@
+
+
+function greet(name, owner) {
+    switch (name) {
+        case owner:
+            return 'Hello boss'
+        case name:
+            return 'Hello guest'
+    }
+}
+
+console.log(greet('Jonah', 'Jonah'))
