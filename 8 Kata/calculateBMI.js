7f17b4b0be52caef0b79019f58a91b9016c2ac2e
Jonah Latham <jonah.latham21@gmail.com>
2019-11-08 19:38:18 -0500

    code

---
+++ 8 Kata/calculateBMI.js
@@ -0,0 +1,14 @@
+function bmi(weight, height) {
+    let bmi = (weight / Math.pow(height, 2))
+    if (bmi <= 18.5) {
+        return 'Underweight'
+    } else if (bmi <= 25.0) {
+        return 'Normal'
+    } else if (bmi <= 30.0) {
+        return 'Overweight'
+    } else {
+        return 'Obese'
+    }
+}
+
+console.log(bmi(80, 1.80))
