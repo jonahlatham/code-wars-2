f27f6ea283b6f4a44b4eefc1cb4453c8db271963
Jonah Latham <jonah.latham21@gmail.com>
2020-03-08 18:59:59 -0400

    code

---
+++ C#/8 Kata/vowelRemover.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Linq;
+public class Kata
+{
+    public static string Shortcut (string input)
+    {
+        char[] result = input.ToCharArray ();
+        return String.Join ("", result.Where (x => x != 'a' && x != 'e' && x != 'i' && x != 'o' && x != 'u'));
+    }
+}
