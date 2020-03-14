f6bba217538924b25268f9872048a6d78538ee55
Jonah Latham <jonah.latham21@gmail.com>
2020-03-13 20:50:37 -0400

    code

---
+++ C#/Edabit/removeFirstAndLast.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static string RemoveFirstLast (string str)
+    {
+        return str.Substring (1) + str.Substring (0, 1).Remove (str.Length - 1);
+    }
+}
