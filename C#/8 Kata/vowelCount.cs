cf1b95a3fde073df67c59d2f4587c563dc6bce8f
Jonah Latham <jonah.latham21@gmail.com>
2020-05-05 12:00:00 +0100

    code

---
+++ C#/8 Kata/vowelCount.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata
+{
+    public static int GetVowelCount (string str)
+    {
+        return str.Count (e => "aeiou".Contains (Char.ToLower (e)));
+    }
+} 
