bd0e62307dc5138c954734eb5e7a7c88c55f8673
Jonah Latham <jonah.latham21@gmail.com>
2020-07-01 12:00:00 +0100

    code

---
+++ C#/8 Kata/removingElements2.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+public static class Kata
+{
+    public static object[] RemoveEveryOther (object[] arr)
+    {
+        // happy coding
+    }
+}
