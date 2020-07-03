da122cb67d4bc68c9c22fac93e3ed8640baf69b1
Jonah Latham <jonah.latham21@gmail.com>
2020-07-03 09:34:40 -0400

    code

--- C#/8 Kata/arrayMadness2.cs
+++ C#/8 Kata/arrayMadness2.cs
@@ -7,4 +7,14 @@ public class Kata
     {
         return a.Sum (x => x * x) > b.Sum (x => x * x * x);
     }
+
+    public static bool ArrayMadness2 (int[] a, int[] b)
+    {
+        return a.Sum (t => Math.Pow (t, 2)) > b.Sum (t => Math.Pow (t, 3));
+    }
+
+    public static bool ArrayMadness (int[] a, int[] b)
+    {
+        return a.Select (i => i * i).Sum () > b.Select (z => z * z * z).Sum ();
+    }
 }
